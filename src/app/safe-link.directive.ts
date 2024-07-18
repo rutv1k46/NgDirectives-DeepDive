10ecf2101ad7740cd15f18fd2ddde77d673c2c99
rutv1k46 <rutviksolanki01@gmail.com>
2024-07-17 23:35:33 -0400

    hostElementRef

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
   selector: "a[appSafeLink]",
@@ -9,6 +9,8 @@ import { Directive, input } from "@angular/core";
 })
 export class SafeLinkDirective {
   queryParam = input("myapp", { alias: "appSafeLink" });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
   constructor() {
     console.log("Safe link directive is active!");
   }
@@ -17,8 +19,9 @@ export class SafeLinkDirective {
     const wantsToLeave = window.confirm("Do you wish to leave the page?");
 
     if (wantsToLeave) {
-      const address = (event.target as HTMLAnchorElement).href;
-      (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam()
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href =
+        address + "?from=" + this.queryParam();
       return;
     }
 
